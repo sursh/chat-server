533ae7bc7cfb0330f62b7d091d9ec3b33a654088
Sasha Laundy <sasha.laundy@gmail.com>
2013-05-02 13:14:55 -0400

    Reset threading

--- chat.py
+++ chat.py
@@ -15,7 +15,6 @@ def recv_all(sock, length):
 
     data = ''
     more = sock.recv(length)
-    # if we're out of incoming data, we're done, close the socket!
     if not more:
         raise EOFError('socket closed %d chars into a %d-char message' % (len(data), length))
     data += more
@@ -26,9 +25,9 @@ def main():
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
         # TODO: allow more than 1 client to connect at a time
-        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 2)
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         s.bind((HOST, PORT))
-        s.listen(2) # TODO: here too
+        s.listen(1) # TODO: here too
 
         print 'Listening for connections at', s.getsockname()
         sc, sockname = s.accept()
@@ -45,5 +44,5 @@ def main():
         sc.close()
 
 if __name__ == '__main__':
-  main()
+    main()
 
