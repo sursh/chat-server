aaa350e4b3a0f3b0d4b6fba41108fa3ac964a3a2
Sasha Laundy <sasha.laundy@gmail.com>
2013-05-02 16:03:43 -0400

    Add master queue and masterSender method

--- chat.py
+++ chat.py
@@ -1,14 +1,40 @@
 #!/usr/bin/env python
 # working on a simple chat client
 
-# TODO
-# dejank the readin function
-# add data structure to keep track of users
+'''
+TODO
+[ ] check to make sure child threads are showing up as expected
+[ ] implement message queue and just print out incoming messages
+[ ] implement message sending to all clients except sender
+[ ] pass along sender info as well as the message
+[ ] add data structure to keep track of users
+'''
 
-import socket, sys
+import sys
+import socket
+import Queue
+import threading
 
 HOST = '127.0.0.1'
 PORT = 1060
+NUM_CLIENTS = 10
+
+# Message queue from all clients
+masterQueue = Queue.Queue()
+
+# DEFINE MASTER SENDER
+class MasterSender(threading.Thread):
+    ''' Pull messages off the masterQueue and send to all clients '''
+    def __init__(self, queue):
+        threading.Thread.__init__(self)
+        self.queue = queue
+
+    def run(self):
+        print "You just initialized a master sender."
+
+# DEFINE MESSAGE PUTTER
+
+# DEFINE MESSAGE GETTER
 
 def recv_all(sock, length):
     ''' Receives the first 'length' chars of a message from 'sock'. '''
@@ -20,24 +46,33 @@ def recv_all(sock, length):
     data += more
     return data
 
+
 def main():
     try:
+        # initialize passive socket at PORT
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-        # TODO: allow more than 1 client to connect at a time
-        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, NUM_CLIENTS)
         s.bind((HOST, PORT))
-        s.listen(1) # TODO: here too
 
-        print 'Listening for connections at', s.getsockname()
-        sc, sockname = s.accept()
+        # TODO create master sender
+        ms = MasterSender(masterQueue)
+        ms.setDaemon(True) # ?
+        ms.start()
 
+        while True:
+            s.listen(NUM_CLIENTS)
+            print 'Listening for connections at', s.getsockname()
+
+            # TODO spin out a new set of client threads instead of creating new object
+            sc, sockname = s.accept()
+
+
+        # TODO MOVE THIS INTO CHILD THREAD
         while True:
             sc.send('> ') # client's chat prompt
             message = recv_all(sc, 1000)
             print '%s says: %s' % (sc.getpeername(), str(message))
 
-            # TODO broadcast new message to all clients
     except KeyboardInterrupt:
         sc.send('!!! Server shutting down.\n')
         print "\nKthxbai, shutting down servers."
